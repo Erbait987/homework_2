108b340048e7de759ab93685f2b01e2c53996cdb
Erbait987 <erbikaiev@gmail.com>
2023-11-17 21:25:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const ColorSquare = (props) => {
+  return (
+    <div
+      style={{
+        width: "100px",
+        height: "100px",
+        backgroundColor: props.color,
+        margin: "10px",
+        cursor: "pointer",
+      }}
+      onClick={props.onClick}
+    ></div>
+  );
+};
 
 function App() {
+  const [square1, setSquare1] = useState("red");
+  const [square2, setSquare2] = useState("blue");
+
+  const handleClick = (num) => {
+    if (num === 1) {
+      setSquare1(square1 === "red" ? "blue" : "red");
+    } else {
+      setSquare2(square2 === "red" ? "blue" : "red");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ColorSquare color={square1} onClick={() => handleClick(1)} />
+      <ColorSquare color={square2} onClick={() => handleClick(2)} />
     </div>
   );
 }
